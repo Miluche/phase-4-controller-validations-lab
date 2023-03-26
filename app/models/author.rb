48f89d238eb848d24dd229d5baf4d56e2a0a98d3
Miluche <meshkipla@gmail.com>
2023-03-26 11:25:28 +0300

    added author and post validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+  validates :name, presence: true,  allow_blank: false
+  validates :email, presence:true, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, allow_blank: false
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, length: {minimum: 100}
 end
